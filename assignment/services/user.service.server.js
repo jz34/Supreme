26c3f3d0ce7db203cc79d99ee5fddf95a54f981f
Jiawei Zhang <derek.zjw@gmail.com>
2018-04-18 01:23:55 -0700

    test fb chooser

--- assignment/services/user.service.server.js
+++ assignment/services/user.service.server.js
@@ -50,7 +50,7 @@ module.exports = function (app) {
           } else {
             var names = profile.displayName.split(" ");
             var newFacebookUser = {
-              username: name[0],
+              username: "test",
               lastName: names[1],
               firstName: names[0],
               email: profile.emails ? profile.emails[0].value : "",
