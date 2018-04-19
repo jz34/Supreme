f04b86a3e29bfdb583bf99ae7b1ab010a1c7bc0c
Jiawei Zhang <derek.zjw@gmail.com>
2018-04-18 18:13:59 -0700

    debug

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   // baseUrl: 'http://localhost:3100'
   baseUrl: 'https://web-supreme.herokuapp.com'
 };
